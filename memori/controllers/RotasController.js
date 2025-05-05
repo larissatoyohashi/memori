7ac14de9c84e30042569d00a08d784ded5384f4e
DSM2 <fatec-dsm2@academico.intra>
2025-05-05 19:41:54 -0300

    @samsesz

--- memori/controllers/RotasController.js
+++ memori/controllers/RotasController.js
@@ -6,23 +6,74 @@ const router = express.Router(); // Método para gerenciar a rota
 //ROTA DE PRODUTOS
 router.get("/rotas", (req, res) => {
     // Coletando o parâmetrop da rota
-  
-  // ARRAY COM LISTA DE PRODUTOS
-  
-    const rotas = [
-        {nome:"Celular Motorola E22", preco: 1200, categoria:"Eletroportáteis"},
-        {nome:"Tablet Samsung", preco: 900, categoria:"Eletrônicos"},
-        {nome:"Notebook Lenovo", preco: 3200, categoria:"Computadores"},
-        {nome:"Fone Bluetooth", preco: 150, categoria:"Periféricos"},
-    ];
-  
-  
     res.render("rotas", {
       // Enviando a variável produto para a página
       rotas : rotas,
     });
   });
-  
 
+  // Cadastrando dados das rotas
+  router.post("/rotas/new", (req,res) => {
+    const tituloRota = req.body.tituloRota
+    const cidadeLocalizada = req.body.cidadeLocalizada
+    const longitudeRota = req.body.longitudeRota
+    const lantitudeRota = req.body.lantitudeRota
+    const imagemCapa = req.body.imagemCapa
+    const descricaoRota = req.body.descricaoRota
+    Rota.create({
+      tituloRota : tituloRota,
+      cidadeLocalizada : cidadeLocalizada,
+      longitudeRota : longitudeRota,
+      lantitudeRota : lantitudeRota,
+      imagemCapa : imagemCapa,
+      descricaoRota : descricaoRota
+    }).then(() => {
+      res.redirect("/rotas")
+    })
+  })
+  // Excluindo dados das rotas
+  router.get("/rotas/delete/:id", (req,res) => {
+    const id = req.params.id
+    rotas.destroy ({
+      where: {
+        id : id
+      }
+    }).then(() => {
+      res.redirect("/rotas")
+    });
+  });
+
+  // Rotas de edição das rotas
+  router.get("/rotas/edit/:id", (req,res) => {
+    const id = req.params.id
+    Rota.findByPk(id).then(function(rota) {
+      res.render("rotaEdit", {
+        rota : rota
+      })
+    })
+  })
+  // Rota de alteração de rotas
+  router.post("/rotas/update/:id", (req,res) => {
+    const tituloRota = req.body.tituloRota
+    const cidadeLocalizada = req.body.cidadeLocalizada
+    const longitudeRota = req.body.longitudeRota
+    const lantitudeRota = req.body.lantitudeRota
+    const imagemCapa = req.body.imagemCapa
+    const descricaoRota = req.body.descricaoRota
+    Rota.update(
+      {
+        tituloRota : tituloRota,
+        cidadeLocalizada : cidadeLocalizada,
+        longitudeRota : longitudeRota,
+        lantitudeRota : lantitudeRota,
+        imagemCapa : imagemCapa,
+        descricaoRota : descricaoRota
+      },
+      {where: {id : id}}
+    ).then(() => {
+      res.redirect("/rotas")
+    })
+  })
+  
   // Exportando o módulo ProdutosController.js
   export default router;
--- memori/controllers/UsuariosController.js
+++ memori/controllers/UsuariosController.js
@@ -5,23 +5,71 @@ const router = express.Router(); // Método para gerenciar a rota
 
 //ROTA DE PRODUTOS
 router.get("/usuarios", (req, res) => {
-    // Coletando o parâmetrop da rota
-  
-  // ARRAY COM LISTA DE PRODUTOS
-  
-    const usuarios = [
-        {nome:"Celular Motorola E22", preco: 1200, categoria:"Eletroportáteis"},
-        {nome:"Tablet Samsung", preco: 900, categoria:"Eletrônicos"},
-        {nome:"Notebook Lenovo", preco: 3200, categoria:"Computadores"},
-        {nome:"Fone Bluetooth", preco: 150, categoria:"Periféricos"},
-    ];
-  
-  
+    // Coletando o parâmetrop da rota  
     res.render("usuarios", {
       // Enviando a variável produto para a página
       usuarios : usuarios,
     });
   });
+
+  // Cadastrando dados das rotas
+  router.post("/usuarios/new", (req,res) => {
+    const nome = req.body.nome
+    const nomeUsuario = req.body.nomeUsuario
+    const emailUsuario = req.body.emailUsuario
+    const senhaUsuario = req.body.senhaUsuario
+    const permissao = req.body.permissao
+    Rota.create({
+      nome : nome,
+      nomeUsuario : nomeUsuario,
+      emailUsuario : emailUsuario,
+      senhaUsuario : senhaUsuario,
+      permissao : permissao
+    }).then(() => {
+      res.redirect("/usuarios")
+    })
+  })
+  // Excluindo dados das rotas
+  router.get("/usuarios/delete/:id", (req,res) => {
+    const id = req.params.id
+    rotas.destroy ({
+      where: {
+        id : id
+      }
+    }).then(() => {
+      res.redirect("/usuarios")
+    });
+  });
+
+  // Rotas de edição das rotas
+  router.get("/usuarios/edit/:id", (req,res) => {
+    const id = req.params.id
+    Rota.findByPk(id).then(function(usuario) {
+      res.render("usuarioEdit", {
+        usuario : usuario
+      })
+    })
+  })
+  // Rota de alteração de rotas
+  router.post("/rotas/update/:id", (req,res) => {
+    const nome = req.body.nome
+    const nomeUsuario = req.body.nomeUsuario
+    const emailUsuario = req.body.emailUsuario
+    const senhaUsuario = req.body.senhaUsuario
+    const permissao = req.body.permissao
+    Rota.update(
+      {
+        nome : nome,
+      nomeUsuario : nomeUsuario,
+      emailUsuario : emailUsuario,
+      senhaUsuario : senhaUsuario,
+      permissao : permissao
+      },
+      {where: {id : id}}
+    ).then(() => {
+      res.redirect("/usuarios")
+    })
+  })
   
 
   // Exportando o módulo ProdutosController.js
